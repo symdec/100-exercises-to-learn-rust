b842ac53bb3b844d5f8d15a2444f56fbe182c63a
Simon Decomble <simon.decomble@inria.fr>
2026-09-03 17:34:09 +0200

    exs/08/00-07

--- exercises/08_futures/01_async_fn/src/lib.rs
+++ exercises/08_futures/01_async_fn/src/lib.rs
@@ -1,4 +1,5 @@
 use tokio::net::TcpListener;
+use tokio::io;
 
 // TODO: write an echo server that accepts incoming TCP connections and
 //  echoes the received data back to the client.
@@ -11,7 +12,11 @@ use tokio::net::TcpListener;
 // - `tokio::net::TcpStream::split` to obtain a reader and a writer from the socket
 // - `tokio::io::copy` to copy data from the reader to the writer
 pub async fn echo(listener: TcpListener) -> Result<(), anyhow::Error> {
-    todo!()
+    loop {
+        let (mut socket, _) = listener.accept().await?;
+        let (mut reader, mut writer) = socket.split();
+        io::copy(&mut reader, &mut writer).await?;
+    }
 }
 
 #[cfg(test)]
--- exercises/08_futures/03_runtime/src/lib.rs
+++ exercises/08_futures/03_runtime/src/lib.rs
@@ -2,6 +2,7 @@
 //  accept connections on both of them concurrently, and always reply to clients by sending
 //  the `Display` representation of the `reply` argument as a response.
 use std::fmt::Display;
+use std::sync::{Arc, RwLock};
 use tokio::io::AsyncWriteExt;
 use tokio::net::TcpListener;
 
@@ -10,7 +11,23 @@ where
     // `T` cannot be cloned. How do you share it between the two server tasks?
     T: Display + Send + Sync + 'static,
 {
-    todo!()
+    let shared_reply = Arc::new(reply);
+    let _ = tokio::spawn(fixed_reply_on(first, Arc::clone(&shared_reply)));
+    let _ = tokio::spawn(fixed_reply_on(second, shared_reply));
+}
+
+pub async fn fixed_reply_on<T>(listener: TcpListener, reply: Arc<T>) -> Result<(), anyhow::Error> 
+where
+    T: Display + Send + Sync + 'static,
+{
+    loop {
+        let (mut socket, _) = listener.accept().await.unwrap();
+        let (_reader, mut writer) = socket.split();
+        writer
+            .write_all(format!("{}", reply).as_bytes())
+            .await
+            .unwrap();
+    }
 }
 
 #[cfg(test)]
